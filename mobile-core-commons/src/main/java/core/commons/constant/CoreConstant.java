01a839cabb100276477f49280e4fb0807cfb7893
nbduc1996 <nbduc1996@gmail.com>
2018-02-23 15:41:26 +0700

    Change 2

--- mobile-core-commons/src/main/java/core/commons/constant/CoreConstant.java
+++ mobile-core-commons/src/main/java/core/commons/constant/CoreConstant.java
@@ -2,26 +2,5 @@ package core.commons.constant;
 
 
 public class CoreConstant {
-    private String name;
-    private int age;
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
     
-    public void cc(){
-        int a = 0;
-    }
 }
